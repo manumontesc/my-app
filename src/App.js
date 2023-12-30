5e6cbdd0b3684d9cfdb7400eb9d536df2ee9907b
examen <manumontesca@gmail.com>
2023-12-30 21:56:05 +0100

    feat(*): aplicación acabada a falta de mejorar el diseño

--- src/App.js
+++ src/App.js
@@ -1,40 +1,147 @@
-// Assuming Graph and City classes are defined in separate files
-
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Graph from './models/Graph';
 import City from './models/City';
-import { CarJourney } from './models/Journey';
+import { AirplaneJourney, BoatJourney, CarJourney, TrainJourney } from './models/Journey';
+import './App.css';
 
-const App = () => {
-  useEffect(() => {
-    // Initialize the graph
-    const myGraph = new Graph();
+const initializeGraph = (graph) => {
+  const paris = new City("Paris");
+  const barcelona = new City("Barcelona");
+  const madrid = new City("Madrid");
+  const rome = new City("Roma");
+  const valencia = new City("Valencia");
+  const malta = new City("Malta");
 
-    // Create cities
-    const paris = new City("Paris");
-    const barcelona = new City("Barcelona");
+  graph.addNodes([paris, barcelona, madrid, rome, valencia, malta]);
 
-    // Add cities to the graph
-    myGraph.addNodes([paris, barcelona]);
-
-    // Create a car journey from Barcelona to Paris
-    const carJourney = new CarJourney({
+  graph.addEdges([
+    new CarJourney({
       origin: barcelona,
       destination: paris,
-      licensePlate: "ABC123" // Replace with an actual license plate
-    });
+      licensePlate: "ABC123"
+    }),
+    new CarJourney({
+      origin: madrid,
+      destination: barcelona,
+      licensePlate: "ABC124"
+    }),
+    new TrainJourney({
+      origin: barcelona,
+      destination: madrid,
+      trainNumber: "4782364",
+      seatNumber: "66"
+    }),
+    new AirplaneJourney({
+      origin: barcelona,
+      destination: rome,
+      flightNumber: "AR8575",
+      seatNumber: "55"
+    }),
+    new AirplaneJourney({
+      origin: rome,
+      destination: malta,
+      flightNumber: "AR8547",
+      seatNumber: "2"
+    }),
+    new AirplaneJourney({
+      origin: malta,
+      destination: rome,
+      flightNumber: "AR6789",
+      seatNumber: "24"
+    }),
+    new AirplaneJourney({
+      origin: madrid,
+      destination: valencia,
+      flightNumber: "AR6739",
+      seatNumber: "22"
+    }),
+    new AirplaneJourney({
+      origin: valencia,
+      destination: madrid,
+      flightNumber: "FV8435",
+      seatNumber: "4"
+    }),
+    new BoatJourney({
+      origin: valencia,
+      destination: malta,
+      boatNumber: "5839"
+    })
+  ]);
+}
+const myGraph = new Graph();
 
-    // Add the car journey to the graph
-    myGraph.addEdge(carJourney);
+const App = () => {
+  const [origin, setOrigin] = useState('');
+  const [destination, setDestination] = useState('');
+  const [results, setResults] = useState([]);
+  const [stages, setStages] = useState([]);
 
-    // Now the graph contains two cities and a car journey connecting them
-    console.log(myGraph);
-  }, []); // Empty dependency array to run the effect only once
+  useEffect(() => {
+    initializeGraph(myGraph);
+  }, []);
+
+  useEffect(() => {
+    console.log(results);
+  }, [results]);
+
+  useEffect(() => {
+   setStages(myGraph.findShortestPath(results[0], results[1]));
+  }, [results]);
+
+  const searchRoute = () => {
+    const originNode = myGraph.findNodeByName(origin);
+    const destinationNode = myGraph.findNodeByName(destination);
+
+    setResults([originNode, destinationNode]);
+  };
+
+
+  const renderStages = () => {
+    if (stages === null) {
+      return(`No existe una ruta de ${results[0].name} a ${results[1].name}`);
+    } else if (stages) {
+        return stages.map((journey, index) => {
+          let journeyInfo = '';
+          if (journey instanceof AirplaneJourney) {
+            journeyInfo = `Coge el avión ${journey.state.flightNumber} con asiento ${journey.state.seatNumber} desde ${journey.state.origin.name} hasta ${journey.state.destination.name}.`;
+          } else if (journey instanceof CarJourney) {
+            journeyInfo = `Coge el coche desde ${journey.state.origin.name} hasta ${journey.state.destination.name} con la matrícula ${journey.state.licensePlate}.`;
+          } else if (journey instanceof TrainJourney) {
+            journeyInfo = `Coge el tren ${journey.state.trainNumber} con asiento ${journey.state.seatNumber} desde ${journey.state.origin.name} hasta ${journey.state.destination.name}.`;
+          } else if (journey instanceof BoatJourney) {
+            journeyInfo = `Coge el barco ${journey.state.boatNumber} desde ${journey.state.origin.name} hasta ${journey.state.destination.name}.`;
+          }
+      
+          return <p key={index}>{journeyInfo}</p>;
+        });
+    }
+  };
 
   return (
-    <div>
-      {/* You can add your React components or UI elements here */}
-    </div>
+    <>
+      <h1 className="app-title">Route Calculator</h1>
+      <div id="search-container">
+        
+          <input
+            type="text"
+            placeholder="Origin"
+            value={origin}
+            onChange={(e) => setOrigin(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Destination"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+          />
+        <button onClick={() => { searchRoute() }}>Search</button>
+        <div className='result'>
+          <h2>Result:</h2>
+          {renderStages()}
+        </div>
+      </div>
+      
+    </>
   );
 };
 
--- src/models/Graph.js
+++ src/models/Graph.js
@@ -16,17 +16,49 @@ class Graph extends React.Component {
   };
 
   addNodes = (nodesArray) => {
-    this.nodes = [...this.nodes, ...nodesArray];
+    this.nodes = [...nodesArray];
   };
 
   addEdges = (edgesArray) => {
-    this.edges = [...this.edges, ...edgesArray];
+    this.edges = [...edgesArray];
   };
 
-  render() {
-    // You can add rendering logic here if needed
-    return null;
+  findNodeByName(name) {
+    
+    return this.nodes.find((node) => node.name === name) || null;
   }
+
+  findShortestPath = (start, end) => {
+    
+    const visited = new Set();
+    const queue = [[start, []]]; 
+  
+    while (queue.length > 0) {
+      const [currentNode, pathSoFar] = queue.shift();
+  
+      if (currentNode === end) {
+        return pathSoFar;
+      }
+  
+      if (!visited.has(currentNode)) {
+        visited.add(currentNode);
+        const neighbors = this.edges
+          .filter((edge) => edge.props.origin === currentNode)
+          .map((edge) => edge.props.destination);
+  
+        for (const neighbor of neighbors) {
+          queue.push([neighbor, [...pathSoFar, this.getEdge(currentNode, neighbor)]]);
+        }
+      }
+    }
+  
+    return null; 
+  };
+  
+  getEdge = (origin, destination) => {
+    return this.edges.find((edge) => edge.props.origin === origin && edge.props.destination === destination);
+  };
+  
 }
 
 export default Graph;
